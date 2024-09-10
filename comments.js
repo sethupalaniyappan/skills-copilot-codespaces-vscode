7508aaf27769c487bee7137bb785211113a26484
sethupalaniyappan <sethu30palani@gmail.com>
2024-09-10 16:56:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,6 @@
-// create web server
+// Create web server
 const express = require('express');
 const app = express();
 
-// create route
-app.get('/comments', (req, res) => {
-  res.send('This is a page for comments');
-});
-
-// start server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+// Import the data
+const comments = require('./data/comments');
